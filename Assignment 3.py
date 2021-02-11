27b1889f7a51c351f6e976eb03e6d1316bfb5b1c
Vivswan Shah <shahvivswan@gmail.com>
2021-02-10 20:17:45 -0500

    refactor

--- Assignment 3.py
+++ Assignment 3.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 
 
-def is_physical(pho):
+def physicality(pho):
     return np.trace(pho)
 
 
@@ -23,21 +23,31 @@ def kron(*args):
 
 def trace_of(density_matrix, remove_qubit):
     num_qubits = int(math.log2(np.shape(density_matrix)[0]))
-    new_matrix = np.zeros((2 ** (num_qubits - 1), 2 ** (num_qubits - 1))).tolist()
+
+    if not (0 <= remove_qubit < num_qubits):
+        raise Exception(f"Invalid remove qubit: "
+                        f"0 <= remove_qubit: {remove_qubit} <= num of qubit in matrix = {num_qubits - 1}")
+
+    new_matrix_shape = (2 ** (num_qubits - 1), 2 ** (num_qubits - 1))
+    new_matrix = np.zeros(new_matrix_shape, dtype=complex)
+    test_matrix = np.zeros(new_matrix_shape)
+
     notation_array = []
     for i in range(0, 2 ** num_qubits):
         notation_array.append("{0:b}".format(i).zfill(num_qubits))
+
     for row_index, row in enumerate(notation_array):
         for col_index, col in enumerate(notation_array):
             if row[remove_qubit] == col[remove_qubit]:
-                new_row = row[0:remove_qubit] + row[remove_qubit + 1:]
-                new_col = col[0:remove_qubit] + col[remove_qubit + 1:]
-                new_row_num = int(new_row, 2)
-                new_col_num = int(new_col, 2)
-                # print(f"{row},{col} -> {new_row},{new_col} -> {density_matrix[row_index][col_index]}")
-                new_matrix[new_row_num][new_col_num] += density_matrix[row_index][col_index]
+                new_row = int(row[0:remove_qubit] + row[remove_qubit + 1:], 2)
+                new_col = int(col[0:remove_qubit] + col[remove_qubit + 1:], 2)
+                new_matrix[new_row][new_col] += density_matrix[row_index][col_index]
+                test_matrix[new_row][new_col] += 1
 
-    return np.array(new_matrix)
+    if not np.all(test_matrix == 2):
+        raise Exception("Not able to Trace properly")
+
+    return new_matrix
 
 
 def main():
@@ -50,26 +60,34 @@ def main():
         [1]
     ])
 
-    # zz = kron(zero, zero)
-    # zo = kron(zero, one)
-    # oz = kron(one, zero)
-    # oo = kron(one, one)
-    #
-    # E_plus = np.array(1 / np.sqrt(2) * (zz + oo))
-    # E_minus = np.array(1 / np.sqrt(2) * (zz - oo))
-    # O_plus = np.array(1 / np.sqrt(2) * (zo + oz))
-    # O_minus = np.array(1 / np.sqrt(2) * (zo - oz))
-    #
-    # E_plus_density = np.matmul(E_plus, E_plus.conjugate().transpose())
-    # E_minus_density = np.matmul(E_minus, E_minus.conjugate().transpose())
-    # O_plus_density = np.matmul(O_plus, O_plus.conjugate().transpose())
-    # O_minus_density = np.matmul(O_minus, O_minus.conjugate().transpose())
-    #
-    # for i in [E_plus_density, E_minus_density, O_plus_density, O_minus_density]:
-    #     for j in [0, 1]:
-    #         new_matrix = trace_of(i, j)
-    #         print(is_physical(new_matrix), purity(new_matrix))
+    print('Question 1')
+    zz = kron(zero, zero)
+    zo = kron(zero, one)
+    oz = kron(one, zero)
+    oo = kron(one, one)
 
+    e_plus = np.array(1 / np.sqrt(2) * (zz + oo))
+    e_minus = np.array(1 / np.sqrt(2) * (zz - oo))
+    o_plus = np.array(1 / np.sqrt(2) * (zo + oz))
+    o_minus = np.array(1 / np.sqrt(2) * (zo - oz))
+
+    density_matrices = {
+        'E_plus_density': np.matmul(e_plus, e_plus.conjugate().transpose()),
+        'E_minus_density': np.matmul(e_minus, e_minus.conjugate().transpose()),
+        'O_plus_density': np.matmul(o_plus, o_plus.conjugate().transpose()),
+        'O_minus_density': np.matmul(o_minus, o_minus.conjugate().transpose())
+    }
+    for i in density_matrices:
+        for j in [0, 1]:
+            new_matrix = trace_of(density_matrices[i], j)
+            print()
+            print(f"Trace of {j} on {i} => "
+                  f"Physical: {abs(np.round(physicality(new_matrix), 1))}, "
+                  f"Purity: {abs(np.round(purity(new_matrix), 1))}")
+            print(new_matrix.round(2))
+
+    print()
+    print('Question 2')
     zzo = kron(zero, zero, one)
     ozz = kron(one, zero, zero)
     zoo = kron(zero, one, one)
@@ -86,7 +104,10 @@ def main():
         xx = [0, 1, 2]
         xx.remove(i)
         print()
-        print(xx, abs(is_physical(after_trace)), abs(purity(after_trace)))
+        print(f"Trace of {i} => phi{xx} => "
+              f"Physical: {abs(physicality(after_trace))}, "
+              f"Purity: {abs(purity(after_trace))}, "
+              f"Fully Entangled: {abs(purity(after_trace)) == 1}")
         print(after_trace)
 
         # for j in [0, 1]:
